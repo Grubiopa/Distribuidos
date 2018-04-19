55d0346f5d4df2ea693b8843dec6bb56a66d7a7c
Grubiopa <gabrielrubiopalacios@gmail.com>
2018-04-19 09:50:03 +0200

    Inicializacion

---
+++ JS/credencialesFlickr.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var api_key = "81c8f7d5b194003abd9a61b6e8230249";
+var user_id = "";
+
+
+
