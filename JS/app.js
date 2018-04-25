a4ddf0581e662c06c23a6d6f89071085489df3eb
Grubiopa <gabrielrubiopalacios@gmail.com>
2018-04-25 13:31:32 +0200

    Todo a app.js
    Todo lo de antony metido

--- JS/app.js
+++ JS/app.js
@@ -1,21 +1,57 @@
-$(function() {
-$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
-	 + api_key + '&user_id=' +user_id + 
-	'&format=json&nojsoncallback=1',
-	mostrar_fotos
-)
+$(function cargaImagenesUsuario() 
+	{
+		$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+			 + api_key + '&user_id=' +user_id + 
+			'&format=json&nojsoncallback=1',
+			function (info){
+				var i;
+				for (i=0;i<info.photos.photo.length;i++) {
+				   var item = info.photos.photo[i];
+				   var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+							  +'/'+item.id+'_'+item.secret+'_m.jpg';
+				   console.debug(url);
+				   $("#imagenes").append($("<img/>").attr("src",url));
+				}
+			}
+		)	
+	}
+);
 
-function mostrar_fotos(info){
-	var i;
-	for (i=0;i<info.photos.photo.length;i++) {
-	   var item = info.photos.photo[i];
-	   var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
-		          +'/'+item.id+'_'+item.secret+'_m.jpg';
-	   console.debug(url);
-	   $("#imagenes").append($("<img/>").attr("src",url));
-    }
+$(function cargarImagenesRecientes() 
+{
+	$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.getRecent&api_key=' 
+		 + api_key + '&user_id=' +user_id + '&per_page=2' +
+		'&format=json&nojsoncallback=1',
+		function (info){
+			var i;
+			for (i=0;i<info.photos.photo.length;i++) {
+				var item = info.photos.photo[i];
+				var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+							+'/'+item.id+'_'+item.secret+'_m.jpg';
+				console.debug(url);
+				$("#imagenesRecientes").append($("<img/>").attr("src",url));
+			}
+		}
+	)
 }
+);
+$(function cargarImagenesTexto() 
+	{
+		$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+			 + api_key + '&user_id=' +user_id + '&text=Sesiones'+
+			'&format=json&nojsoncallback=1',
+			function (info){
+				var i;
+				for (i=0;i<info.photos.photo.length;i++) {
+				   var item = info.photos.photo[i];
+				   var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+							  +'/'+item.id+'_'+item.secret+'_m.jpg';
+				   console.debug(url);
+				   $("#imagenesTexto").append($("<img/>").attr("src",url));
+				}
+			}
+		)
+	}
+);	
 
-}
-)												
-                
+
